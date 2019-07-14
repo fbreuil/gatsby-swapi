90d1a1775a4db32db69744525f18ff758720a257
Felipe <felipe.breuil@gmail.com>
2019-07-13 22:43:56 -0300

    Update alt do gif de loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ import bb8 from '../assets/images/bb8.gif';
 const Loading = () => (
   <section className="loading">
     <h1 className="title">Loading...</h1>
-    <img src={bb8} className="image"/>
+    <img src={bb8} className="image" alt="Imagem de loading" />
   </section>
 
 );
